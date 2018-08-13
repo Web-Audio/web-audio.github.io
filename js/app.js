a17c386a07ae2ef3be5921e4f09318f27af234a9
Caterina Paun <caterina.paun@gmail.com>
2018-08-12 21:41:24 -0700

    Add compatibility check, implement start adn clear functions

--- js/app.js
+++ js/app.js
@@ -1,70 +1,86 @@
 // Start with init() function on page load
 window.addEventListener('load', init, false);
-
-// Check for browser compatiblity with Web Audio API
-function init() {
-  try {
-    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-    let context = new SpeechRecognition();
-    console.log(context);
-  }
-  catch(e) {
-    alert('Web Audio API is not supported in this browser');
-  }
-}
-
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+// console.log(stopwords);
 
 let arr = [];
 let textstring = "";
 
-const recognition = new SpeechRecognition();
-recognition.interimResults = true;
-recognition.lang = 'en-US';
+// Check for browser compatiblity with Web Audio API & microphone access
+function init() {
+  let testSpeech;
 
+  try {
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    testSpeech = new SpeechRecognition();
+    console.log(testSpeech);
+  }
+  catch(e) {
+    alert('Web Audio API is not supported in this browser');
+  }
 
-let p = document.createElement('p');
-const words = document.querySelector('.words');
-words.appendChild(p);
-
-recognition.addEventListener('result', e => {
-  const transcript = Array.from(e.results)
-    .map(result => result[0])
-    .map(result => result.transcript)
-    .join('');
-
-    const replaceScript = transcript.replace(/hello/gi, 'HELLO')
-                          .replace(/like/gi, 'LIKE')
-                          .replace(/dog/gi, '🐕')
-                          .replace(/aced/gi, '💯')
-                          .replace(/emoji|emojis/gi, '😁')
-                          .replace(/testing/gi, 'TESTING');
-    p.textContent = replaceScript;
-
-    if (e.results[0].isFinal) {
-      p = document.createElement('p');
-      words.appendChild(p);
-      textstring += transcript + " ";
-      console.log(textstring);
+  testSpeech.onerror = function(event) {
+    if (event.error == "no-speech") {
+      alert("Error: No speech available.");
     }
+    if (event.error == "audio-capture") {
+      alert("Error: No microphone available for audio capture.");
+    }
+  };
 
-    var stopWords = /(?:^|\s+)(?:the|and|a|or|in|on)(?=\s+|$)/gi;
-    arr = textstring.replace(stopWords, "").trim();
+}
 
-    console.log("===============????");
-    console.log("teststring", arr);
+function startSpeech() {
 
-    arr = textstring.split(" ");
-    printResults();
-});
+  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-recognition.addEventListener('end', recognition.start);
+  const recognition = new SpeechRecognition();
+  recognition.interimResults = true;
+  // Set the language the English (US); this can be set to seveal other languages and dialets
+  recognition.lang = 'en-US';
 
-recognition.start();
+  let p = document.createElement('p');
+  const paper = document.querySelector('.paper');
+  paper.appendChild(p);
 
-recognition.onspeechend = function() {
-  recognition.stop();
-  console.log('Speech recognition has stopped.');
+  recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('');
+
+      const replaceScript = transcript.replace(/hello/gi, 'HELLO')
+                            .replace(/like/gi, 'LIKE')
+                            .replace(/dog/gi, '🐕')
+                            .replace(/aced/gi, '💯')
+                            .replace(/emoji|emojis/gi, '😁');
+      p.textContent = replaceScript;
+
+      if (e.results[0].isFinal) {
+        p = document.createElement('p');
+        paper.appendChild(p);
+        textstring += transcript + " ";
+        console.log(textstring);
+      }
+
+      var stopWords = /(?:^|\s+)(?:the|and|a|or|in|on)(?=\s+|$)/gi;
+      arr = textstring.replace(stopWords, "").trim();
+
+      console.log("===============????");
+      console.log("teststring", arr);
+
+      arr = textstring.split(" ");
+      printResults();
+  });
+
+
+  recognition.addEventListener('end', recognition.start);
+
+  recognition.start();
+
+  recognition.onspeechend = function() {
+    recognition.stop();
+    console.log('Speech recognition has stopped.');
+  }
 }
 
 // Print the word counts for each word
@@ -90,6 +106,11 @@ function printResults() {
       document.getElementById("analyticsCount").innerHTML += output + "<br/>";
     }
   }
+}
+
+function clearSpeech() {
+  var node = document.getElementById("paper");
+  node.innerHTML = "";
 }
 
 function analyzeSpeech() {
